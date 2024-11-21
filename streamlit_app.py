9a32def1660b525954cedca8a6d8791c9f2706cc
elhamod <55500282+elhamod@users.noreply.github.com>
2024-11-20 19:59:46 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,6 +57,7 @@ if prompt := st.chat_input("What is up?"):
     # Stream the response to the chat using `st.write_stream`, then store it in 
     # session state.
     st.chat_message("assistant").write(response)
+    st.write(response)
     # with st.chat_message("assistant"):
     #     st.markdown(response)
         # response = st.write(response)
